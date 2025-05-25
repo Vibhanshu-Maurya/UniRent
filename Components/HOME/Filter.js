28eb4a10d44294064b423291f65a975315c4552b
Vibhanshu-Maurya <vibhanshumaurya90@gmail.com>
2025-05-25 19:14:46 +0530

    i have changed in text

--- Components/HOME/Filter.js
+++ Components/HOME/Filter.js
@@ -4,7 +4,7 @@ import React from 'react'
 const Filter = () => {
   return (
     <View>
-      <Text style={{ fontSize: 20, fontWeight: 'bold' }}>fILTER WITH CATEGORY</Text>
+      <Text style={{ fontSize: 20, fontWeight: 'bold' }}>Vibhanshu maurya is the best code in the worldand this is very beautiful t hing in world and what i can do for you tell me about this things and my college is shepa which is very bad colleg in varanasi</Text>
     </View>
   )
 }
