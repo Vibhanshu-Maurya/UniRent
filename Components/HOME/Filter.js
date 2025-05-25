58e6ade0c6c39c4845220c3770fa8a96eef9b379
Vibhanshu-Maurya <vibhanshumaurya90@gmail.com>
2025-05-25 19:02:53 +0530

    UniRent

--- Components/HOME/Filter.js
+++ Components/HOME/Filter.js
@@ -4,9 +4,9 @@ import React from 'react'
 const Filter = () => {
   return (
     <View>
-      
+      <Text style={{ fontSize: 20, fontWeight: 'bold', margin: 10 }}>fILTER WITH CATEGORY</Text>
     </View>
   )
 }
 
-export default Filter 
+export default Filter;
